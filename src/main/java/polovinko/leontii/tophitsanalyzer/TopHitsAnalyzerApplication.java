3fb9137ae6ad284eeb8d49b6ec348227aec11f88
Leontii Polovinko <half.leon11@gmail.com>
2022-06-07 17:50:30 +0300

    init

---
+++ src/main/java/polovinko/leontii/tophitsanalyzer/TopHitsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package polovinko.leontii.tophitsanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopHitsAnalyzerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TopHitsAnalyzerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/polovinko/leontii/tophitsanalyzer/TopHitsAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package polovinko.leontii.tophitsanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopHitsAnalyzerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
